e796533efe82944c9b49122afbb56df3c4e5cb04
Yiduard Andres Bolivar Ruiz <yiduard.bolivar@panamericana.local>
2021-10-26 11:18:51 -0500

    Primeras paginas creadas

---
+++ js.js
@@ -0,0 +1,32 @@
+//NAVBAR//
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+//slider
+$(document).ready(function(){
+   $('.slider').slider(
+   {
+     indicators:false,
+     height: 600,
+     transition:200,
+     interval: 5000
+   });
+    });
+ // Pause - start slider
+$(document).ready(function(){
+    $(".slider").mouseenter(function(){
+      $('.slider').slider('pause');
+    });
+      $(".slider").mouseleave(function(){
+        $('.slider').slider('start');
+    });
+});
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+$(document).ready(function(){
+  $('.collapsible').collapsible( );
+});
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
