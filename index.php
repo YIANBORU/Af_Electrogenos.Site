a40e0bf45b896032d0218bc7613e0e10aa62d543
Yiduard Andres Bolivar Ruiz <yiduard.bolivar@panamericana.local>
2021-10-26 17:22:36 -0500

    se avanza en la pagina principal y se deja materialize.css y no ...min.css

--- index.php
+++ index.php
@@ -7,20 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>AF Electrogenos - Plantas Electricas</title>
 </head>
-
 <body>
-    
-        <?php
-        include "parts/navbar.html";
-        include "parts/slider.html";
-        include "parts/section1.html";
-        include "parts/section2.html";
-        include "parts/footer.html";
-        ?>
+
+    <?php
+    include "parts/navbar.html";
+    include "parts/slider.html";
+    include "parts/section1.html";
+    include "parts/section2.html";
+    include "parts/footer.html";
+    ?>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript" src="js.js"></script>
--- js.js
+++ js.js
@@ -1,32 +1,31 @@
 //NAVBAR//
-$(document).ready(function(){
-  $('.sidenav').sidenav();
+$(document).ready(function() {
+    $('.sidenav').sidenav();
 });
 //slider
-$(document).ready(function(){
-   $('.slider').slider(
-   {
-     indicators:false,
-     height: 600,
-     transition:200,
-     interval: 5000
-   });
+$(document).ready(function() {
+    $('.slider').slider({
+        indicators: true,
+        height: 700,
+        transition: 200,
+        interval: 5000
     });
- // Pause - start slider
-$(document).ready(function(){
-    $(".slider").mouseenter(function(){
-      $('.slider').slider('pause');
+});
+// Pause - start slider
+$(document).ready(function() {
+    $(".slider").mouseenter(function() {
+        $('.slider').slider('pause');
     });
-      $(".slider").mouseleave(function(){
+    $(".slider").mouseleave(function() {
         $('.slider').slider('start');
     });
 });
-$(document).ready(function(){
-  $('.carousel').carousel();
+$(document).ready(function() {
+    $('.carousel').carousel();
 });
-$(document).ready(function(){
-  $('.collapsible').collapsible( );
+$(document).ready(function() {
+    $('.collapsible').collapsible();
 });
-$(document).ready(function(){
-  $('.parallax').parallax();
+$(document).ready(function() {
+    $('.parallax').parallax();
 });
