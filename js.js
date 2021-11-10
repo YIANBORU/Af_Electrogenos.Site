3fef8a1c844b09e00469c4dd6f6b3a7a6af2063e
Yiduard Andres Bolivar Ruiz <yiduard.bolivar@panamericana.local>
2021-11-10 14:11:10 -0500

    se avanza con formulario de contacto en el modal1

--- js.js
+++ js.js
@@ -31,23 +31,71 @@ $(document).ready(function() {
 // funcion para que el carousel este en modo autoplay
 $('.carousel').carousel();
 setInterval(function() {
-  $('.carousel').carousel('next');
+    $('.carousel').carousel('next');
 }, 3000); // every 3 seconds
 
 $('.carousel').carousel();
 setInterval(function() {
-  $('#clientesC').carousel('next');
-}, 3000); 
+    $('#clientesC').carousel('next');
+}, 3000);
 
-$(document).ready(function(){
-    $('.carousel').carousel(
-        {
-            duration: 800
-        }
-    );
-  });
+$(document).ready(function() {
+    $('.carousel').carousel({
+        duration: 800
+    });
+});
 
-  
-  $(document).ready(function(){
+
+$(document).ready(function() {
     $('.modal').modal();
-  });
+});
+
+// selection
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function() {
+    $('select').formSelect();
+});
+
+//autocomplete
+$(document).ready(function() {
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Arauca": null,
+            "Armenia": null,
+            "Barranquilla": null,
+            "Bogotá D.C.": null,
+            "Bucaramanga": null,
+            "Cali": null,
+            "Cartagena": null,
+            "Cúcuta": null,
+            "Florencia": null,
+            "Ibagué": null,
+            "Leticia": null,
+            "Manizales": null,
+            "Medellín": null,
+            "Mitú": null,
+            "Mocoa": null,
+            "Montería": null,
+            "Neiva": null,
+            "Pasto": null,
+            "Pereira": null,
+            "Popayán": null,
+            "Quibdó": null,
+            "Riohacha": null,
+            "San Andrés": null,
+            "San José del Guaviare": null,
+            "Santa Marta": null,
+            "Sincelejo": null,
+            "Tunja": null,
+            "Valledupar": null,
+            "Villavicencio": null,
+            "Yopal": null,
+        },
+    });
+});
